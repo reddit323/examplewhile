711478b879d8ab94724db2af87880e6a133619be
raul prudente <raulprudente2016@icloud.com>
2023-05-22 13:48:01 -0400

    hj

---
+++ examplewhile/src/examplewhile/Examplewhile.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examplewhile;
+
+import java.util.Scanner;
+public class Examplewhile {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+       int n;
+       int p;
+       System.out.print("enter value:");
+       p=input.nextInt();
+       
+       System.out.print("enter number:");
+       n=input.nextInt();
+       
+       while(p<=n){
+           p++;
+           System.out.println(p);
+           
+       }
+       while(n<=p){
+           n+=2;
+           System.out.println(n);
+       }
+       
+    
+    }
+    
+}
